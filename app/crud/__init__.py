4b3504f00a559e0836fae0e4cd2a6421ddfb3000
Ольга Пырьянова <pyryanova@mail.ru>
2025-11-05 10:52:50 +0300

    «1.0»

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,31 +1,7 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from app.crud.charity_project import charity_project_crud
+from app.crud.donation import donation_crud
 
-from app.models.charity_project import CharityProject
-from app.schemas.charity_project import CharityProjectCreate
-
-
-class CharityProjectCRUD:
-    async def get_project_by_name(
-        self,
-        session: AsyncSession,
-        project_name: str
-    ) -> CharityProject | None:
-        result = await session.execute(
-            select(CharityProject).where(CharityProject.name == project_name)
-        )
-        return result.scalars().first()
-
-    async def create(
-        self,
-        session: AsyncSession,
-        obj_in: CharityProjectCreate
-    ) -> CharityProject:
-        new_project = CharityProject(**obj_in.model_dump())
-        session.add(new_project)
-        await session.commit()
-        await session.refresh(new_project)
-        return new_project
-
-
-charity_project_crud = CharityProjectCRUD()
+__all__ = [
+    'charity_project_crud',
+    'donation_crud',
+]
