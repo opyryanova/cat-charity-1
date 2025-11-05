875c632f2831433eb37197c13efcc36a3805501e
Ольга Пырьянова <pyryanova@mail.ru>
2025-11-05 10:25:51 +0300

    «1.0»

--- app/core/config.py
+++ app/core/config.py
@@ -2,9 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_title: str = 'QRKot'
-    description: str = 'Благотворительный фонд помощи котам'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    app_title: str
+    description: str
+    app_version: str
+    database_url: str
+    secret: str
+    first_superuser_email: str
+    first_superuser_password: str
 
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
